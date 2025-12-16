691c9673af4edec68dbf1e05977cb99eaa142a91
jaydengonzalezgu347-hue <jayden.gonzalezgu347@students.cmcss.net>
2025-12-16 12:44:52 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,20 @@
+allowance += 3
+allowance += 5
 
+# You bought candy
+allowance -= 4
+
+# Week 2: Parents gave you a bonus! They doubled your allowance for working hard
+bonus *= 2
+
+# You mowed the lawn
+allowance += 8
+
+# You bought a new game
+allowance -= 15
+
+# Week 3: You decided to put half your money in savings
+savings = allowance // 2
+
+# Print final allowance
+print("Allowance: ${allowance}")
